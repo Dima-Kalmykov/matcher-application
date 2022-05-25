f6de52f3440ee7b4ebb24ac664ddd11da7fd130d
Dima-Kalmykov <dekalmykov@edu.hse.ru>
2022-05-25 17:22:25 +0300

    First deploy

---
+++ src/main/kotlin/com/example/matcherapplication/MatcherApplication.kt
@@ -0,0 +1,11 @@
+package com.example.matcherapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatcherApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatcherApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/matcherapplication/MatcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.matcherapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatcherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
